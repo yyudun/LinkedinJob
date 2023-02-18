e6dff06ca8df5aedc3a749c2b712ddae103de587
Y. Udun <113389799+yyudun@users.noreply.github.com>
2023-02-18 20:39:17 +0300

    adding scraping functions

--- linkedinScraping.R
+++ linkedinScraping.R
@@ -50,3 +50,60 @@ scrollToBottom <- function(){
 }
 
 scrollToBottom()
+
+#getting links----
+job_cards <- remDr$findElements(using = 'tag name', 'a') 
+job_links <- lapply(job_cards, function(x) x$getElementAttribute('href')) %>%
+  unlist()
+
+#filtering only the job links
+job_links <- as.data.frame(job_links)
+
+job_links <- job_links %>% 
+  filter(str_detect(string = job_links, pattern = "https://tr.linkedin.com/jobs/view"))
+
+#creating an empty df for criteria
+criteria <- character(0)
+url <- character(0) 
+explanation <- character(0)
+title <- character(0)
+company <- character(0)
+
+kriterler <- tibble(criteria, url, explanation, title)
+
+#functions for scraping ----
+getCriteria <- function(link, kriterler){
+  remDr$navigate(link)
+  Sys.sleep(1) #wait for it to open properly
+  #get criteria card
+  try({
+    job_criteria <- remDr$findElements(using = 'class name', 'description__job-criteria-list')
+    job_criteria <- lapply(job_criteria, function(x) x$getElementText()) %>% unlist()
+    
+    show_more    <- remDr$findElement(using = 'xpath',
+                                      '//*[@id="main-content"]/section[1]/div/div/section[1]/div/div/section/button[1]')
+    show_more$clickElement()
+    
+    explanation <- remDr$findElements( using = "class name", 'description__text--rich')
+    explanation <- lapply(explanation, function(x) x$getElementText()) %>% unlist()
+    
+    title <- remDr$findElement(using = 'tag name', 'h1')
+    title <- title$getElementText() %>% unlist()
+    
+    holder_vec <- c(job_criteria, link, explanation, title)
+    holder_df  <- as.data.frame(t(holder_vec))
+    colnames(holder_df) <- c("criteria", "url", "explanation", "title")
+    kriterler  <- rbind(kriterler, holder_df)}, silent = T)
+  
+  return(kriterler)
+}
+
+scraping <-function(url, kriterler) {
+  i <- 1
+  while(length(url) != i){
+    link = url[i]
+    kriterler <- getCriteria(link, kriterler)
+    i <- i + 1
+  }
+  return(kriterler)
+}
